440757f10be6844c603add1fde32443a82ca3679
Erkan Özcanoglu <ozcanogerkan12@gmail.com>
2023-11-07 14:57:51 +0300

    file editing

--- eGuide.AdminUI/src/app/app-routing.module.ts
+++ eGuide.AdminUI/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { StationsComponent } from './screens/stations/stations.component';
 import { SocketComponent } from './screens/socket/socket.component';
 import { MapComponent } from './components/map/map.component';
+import { SettingsComponent } from './screens/settings/settings.component';
 
 const routes: Routes = [
   { path: 'station', component: StationsComponent },
   { path: 'socket', component: SocketComponent },
   { path: 'map', component: MapComponent },
+  { path: 'settings', component: SettingsComponent },
 ];
 
 @NgModule({
