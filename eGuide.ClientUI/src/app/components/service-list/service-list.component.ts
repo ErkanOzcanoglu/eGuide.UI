d13fd2f677c114c0ead5d1db8bc8b480238bcdad
Erkan Özcanoglu <ozcanogerkan12@gmail.com>
2023-11-19 13:14:27 +0300

    client service screen

--- eGuide.ClientUI/src/app/components/service-list/service-list.component.ts
+++ eGuide.ClientUI/src/app/components/service-list/service-list.component.ts
@@ -1,5 +1,5 @@
 import { ServiceService } from './../../services/service.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Service } from 'src/app/models/service';
 
 @Component({
@@ -9,11 +9,24 @@ import { Service } from 'src/app/models/service';
 })
 export class ServiceListComponent implements OnInit {
   service?: Service[];
+  width?: number;
 
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.getWindowWidth();
+  }
   constructor(private serviceService: ServiceService) {}
 
   ngOnInit(): void {
     this.getServices();
+  }
+
+  getWindowWidth() {
+    if (window.innerWidth < 640) {
+      this.width = 100;
+    } else if (window.innerWidth > 640) {
+      this.width = 50;
+    }
   }
 
   getServices() {
